dec1bcc8cb539f8d05547b80fc6ec3be8795058b
Yanina Carletti <mi mailyanina.carletti@gmail.com>
2026-07-28 18:03:00 -0300

    Configure GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
 export default defineConfig({
+
+  site: "https://yaninacarletti.github.io",
+
   integrations: [mdx()]
+
 });
